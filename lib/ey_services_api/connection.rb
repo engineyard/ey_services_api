b70c6339d0b4ed0c9846f04c299e97561f566f91
Thorben Schröder & Jacob Burkhart <tschroder@engineyard.com>
2011-07-28 14:29:50 -0700

    invoices are good now

--- lib/ey_services_api/connection.rb
+++ lib/ey_services_api/connection.rb
@@ -55,20 +55,25 @@ module EY
           'USER_AGENT' => "Lisonja"}
 
       def register_service(registration_url, params)
-        response = self.client.post(registration_url, STANDARD_HEADERS, {:service => params}.to_json)
-
-        handle_response(registration_url, response) do
+        post(registration_url, :service => params) do |json_body, response_location|
           service = Service.new(params)
           service.connection = self
-          service.url = response["Location"]
+          service.url = response_location
           service
         end
+
+        # response = self.client.post(registration_url, STANDARD_HEADERS, {:service => params}.to_json)
+        # 
+        # handle_response(registration_url, response) do
+        #   service = Service.new(params)
+        #   service.connection = self
+        #   service.url = response["Location"]
+        #   service
+        # end
       end
 
       def get_service(url)
-        response = self.client.get(url, STANDARD_HEADERS)
-        service = nil
-        handle_response(url, response) do |json_body|
+        response = get(url) do |json_body|
           service = Service.new(json_body["service"])
           service.connection = self
           service.url = url
@@ -77,28 +82,68 @@ module EY
       end
 
       def update_service(url, params)
-        response = self.client.put(url, STANDARD_HEADERS, {:service => params}.to_json)
-        handle_response(url, response)
+        put(url, :service => params)
       end
 
       def destroy_service(url)
-        response = self.client.delete(url, STANDARD_HEADERS)
-        handle_response(url, response)
+        delete(url)
       end
 
       #Jacob: will refactor if reach 200 lines!
 
       def send_message(url, message)
-        response = self.client.post(url, STANDARD_HEADERS, {:message => message.to_hash}.to_json)
-        handle_response(url, response)
+        post(url, :message => message.to_hash)
+      end
+
+      def send_invoice(invoices_url, invoice)
+        # #TODO: charge per compliment generator?
+        # invoice_params = {
+        #   :invoice => {
+        #     :total_amount_cents => total_price,
+        #     :line_item_description => "For service from #{last_billed_at} to #{billing_at}, "+
+        #                               "includes #{compliment_generators.size} compliment generators."
+        #   }
+        # }
+        post(invoices_url, :invoice => invoice.to_hash)
+        # response = RestClient.post(
+        #                     invoices_url,
+        #                     {:invoice => invoice.to_hash}.to_json,
+        #                     :content_type => :json,
+        #                     :accept => :json, :user_agent => "Lisonja")
+        # response_data = JSON.parse(response.body)
+        #TODO: do something with the response?
+        #TODO: test that you can't bill a different customer
+        #WHEN testing: handling what happens when you attempt to bill $0 or a negative amount?
       end
 
       protected
+
+      def request(method, url, body = nil, &block)
+        response = client.send(method, url, STANDARD_HEADERS, body.to_json)
+        handle_response(url, response, &block)
+      end
+
+      def post(url, body, &block)
+        request(:post, url, body, &block)
+      end
+      
+      def put(url, body, &block)
+        request(:put, url, body, &block)
+      end
+      
+      def delete(url, &block)
+        request(:delete, url, &block)
+      end
+
+      def get(url, &block)
+        request(:get, url, &block)
+      end
+
       def handle_response(url, response)
         case response.status
         when 200, 201
           json_body = JSON.parse(response.body)
-          yield json_body if block_given?
+          yield json_body, response["Location"] if block_given?
         when 404
           raise NotFound.new(url)
         when 400
